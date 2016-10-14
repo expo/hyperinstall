c4f09da2fb50e16546f4e2bc2d14fe4347a22b1d
James Ide <ide@exp.host>
2016-10-14 19:17:25 +0000

    Run yarn --pure-lockfile
    This way Hyperinstall won't overwrite lockfiles.
    
    fbshipit-source-id: d34e268

--- src/yarn.js
+++ src/yarn.js
@@ -1,5 +1,8 @@
 import { execAsync } from './exec';
 
 export function execYarnInstallAsync(packagePath) {
-  return execAsync('yarn', [], { cwd: packagePath, stdio: 'inherit' });
+  return execAsync('yarn', ['--pure-lockfile'], {
+    cwd: packagePath,
+    stdio: 'inherit',
+  });
 }
